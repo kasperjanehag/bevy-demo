b28983e004a9c4b0be68577397ad42e5111ec6c9
Kasper Janehag <kasper.janehag@gmail.com>
2023-12-26 17:19:43 +0100

    Added advanced scheduling

--- src/debug.rs
+++ src/debug.rs
@@ -1,10 +1,12 @@
 use bevy::prelude::*;
 
+use crate::schedule::InGameSet;
+
 pub struct DebugPlugin;
 
 impl Plugin for DebugPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, print_position);
+        app.add_systems(Update, print_position.after(InGameSet::EntityUpdates));
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
+mod asset_loader;
+mod asteroids;
 mod camera;
+mod collision_detection;
 mod debug;
+mod despawn;
 mod movement;
+mod schedule;
 mod spaceship;
 
 use bevy::prelude::*;
+
+use asset_loader::AssetLoaderPlugin;
+use asteroids::AsteroidPlugin;
 use camera::CameraPlugin;
+use collision_detection::CollisionDetectionPlugin;
 use debug::DebugPlugin;
+use despawn::DespawnPlugin;
 use movement::MovementPlugin;
+use schedule::SchedulePlugin;
 use spaceship::SpaceshipPlugin;
 
 fn main() {
@@ -19,9 +30,14 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         // User defined plugins.
+        .add_plugins(AssetLoaderPlugin)
         .add_plugins(MovementPlugin)
-        .add_plugins(DebugPlugin)
         .add_plugins(SpaceshipPlugin)
+        .add_plugins(AsteroidPlugin)
         .add_plugins(CameraPlugin)
+        .add_plugins(CollisionDetectionPlugin)
+        .add_plugins(DespawnPlugin)
+        .add_plugins(SchedulePlugin)
+        // .add_plugins(DebugPlugin)
         .run();
 }
